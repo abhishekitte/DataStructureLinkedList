22728b64f3b78807ee2ee5961ccfb4a5159e4d9f
ABHI <abhishekitte11@gmail.com>
2021-09-22 14:16:23 +0530

    [ABHISHEK] Added a simple link list

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureLinkedList.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LinkedList < int > list= new LinkedList<int>();
+            //list.AddLast(56);
+            //list.AddLast(30);
+            //list.AddLast(70);
+
+            CustomLinkedList customLinkedList = new CustomLinkedList();
+            customLinkedList.InsertLast(56);
+            customLinkedList.InsertLast(30);
+            customLinkedList.InsertLast(70);
+            customLinkedList.Display();
+            Console.ReadLine();
+        }
+    }
+}
